13fca987a7f1ba718d5d9f47db72446b05a1c253
Mrown <70544258+Mrown@users.noreply.github.com>
2020-09-08 12:57:36 +0200

    Add files via upload

--- isprime.js
+++ isprime.js
@@ -1,5 +1,5 @@
 const isPrime = function (x) {
-    for (var i = 2; i < x; i += 1) {
+    for (var i = 3; i <Math.sqrt (x); i += 2) {
         if (x % i === 0) {
             return false;
         }
